acedbdc9ad21b64c3ef94585217decbed21dc615
Luca Maletti <luca.maletti@external.eni.com>
2021-07-04 22:55:57 +0200

    Refactoring and reassembling

---
+++ TestOCR/Model/IEntityToFind.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestOCR.Model
+{
+    interface IEntityToFind
+    {
+        public  void CheckValue(string textExtracted);
+
+        public  string ValueToString();
+    }
+}
--- TestOCR/Program.cs
+++ TestOCR/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 using System;
-using System.Linq;
+
 using log4net;
 using log4net.Config;
 using System.IO;
@@ -10,52 +10,85 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 
+using Microsoft.Extensions.Hosting;
+using TestOCR.Model;
+
 namespace TestOCR
 {
     class Program
     {
 
         // Add your Computer Vision subscription key and endpoint
-        static string fatturePath = "C:\\Users\\l.maletti\\Documents\\Miei\\AcquisizioneFatture\\Fatture\\";
+        static string fatturePath = "C:\\Users\\l.maletti\\Documents\\Miei\\AcquisizioneFatture\\Fatture\\Test\\";
         static string csvFilename = "data.csv";
 
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            ExecuteMain();
+
+            return host.RunAsync();        
+        }
+
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args);
+
+
+        static void ExecuteMain()
+        {
+            //Set up logging
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
             var _log4net = log4net.LogManager.GetLogger(typeof(Program));
 
-            Console.WriteLine("BILLING ACQUISITION");
-            OCRClient ocrClient = new OCRClient();
-            ocrClient.Authenticate();
-
-            using (StreamWriter swCsv = new StreamWriter(fatturePath + csvFilename, false))
+            try
             {
-                swCsv.WriteLine("Numero Fattura;Data Emissione;Importo");
+                _log4net.Info("BILLING ACQUISITION -> START");
+
+                //Start up client
+                OCRClient ocrClient = new OCRClient();
+                ocrClient.Authenticate();
+
+                StringBuilder sbCsvContent = new StringBuilder();
+                sbCsvContent.AppendLine("Numero Fattura;Data Emissione;Importo");
+
+                //Get billing files
                 DirectoryInfo dirInfo = new DirectoryInfo(fatturePath);
                 FileInfo[] arrFi = dirInfo.GetFiles("*.PDF");
-                Console.WriteLine("Number of files: " + arrFi.Length.ToString());
+                _log4net.InfoFormat("Number of files: {0}", arrFi.Length);
 
                 foreach (FileInfo fi in arrFi)
                 {
+                    //Read file and extract content
                     Task<List<EntityToFind>> tskReadFile = ocrClient.ReadFile(fi);
                     tskReadFile.Wait();
 
-                    StringBuilder sb = new StringBuilder(fi.Name.Split('.')[0]);
-                    tskReadFile.Result.ForEach(x => sb.Append(";" + x.ValueToString()));
-                    swCsv.WriteLine(sb.ToString());
-                    swCsv.Flush();
+                    StringBuilder sbRow = new StringBuilder(fi.Name.Split('.')[0]);
+                    tskReadFile.Result.ForEach(x => sbRow.Append(";" + x.ValueToString()));
+                    sbCsvContent.AppendLine(sbRow.ToString());
                     Thread.Sleep(6000);
-                }           
+                }
+
+                //Write output csv
+                string csvFilePath = fatturePath + csvFilename;
+                _log4net.InfoFormat("Writing final csv file: {0}", csvFilePath);
+                using (StreamWriter swCsv = new StreamWriter(csvFilePath, false))
+                {
+                    swCsv.Write(sbCsvContent.ToString());
+                }
+
+                _log4net.Info("BILLING ACQUISITION -> END");
             }
-            Console.WriteLine("END");
+            catch (Exception err)
+            {
+                _log4net.Error("Error during billing acquisition;", err);
+            }
         }
 
-        
 
-
-       
     }
 
    
